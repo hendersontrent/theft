e337e51e260238705490aa408c4b57200c3c4c63
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-03-26 21:31:59 +1100

    CHanges to PDF automation

--- R/print_bayes_outputs.R
+++ R/print_bayes_outputs.R
@@ -1,25 +1,63 @@
-#' Produce an R Markdown report of all the model fit and diagnostic plots for a Bayesian classification program
-#' @import rmarkdown
-#' @param model a model output from one of the Bayes function arguments contained in sawlog
-#' @return a R Markdown document saved in the user-specified location
+#' Produce a PDF report of all the model fit and diagnostic plots for a Bayesian classification program
+#' @import ggplot2
+#' @import bayesplot
+#' @importFrom Cairo CairoPNG
+#' @param model a model output from one of the Bayes function arguments contained in {sawlog}
+#' @return a PDF document saved in the current work directory
 #' @author Trent Henderson
 #' @export
 #' @examples
 #' \dontrun{
-#' print_bayes_outputs(model, filepath)
+#' print_bayes_outputs(model)
 #' }
 #'
 
 print_bayes_outputs <- function(model){
   
-  #------------ Checks ------------
+  path <- getwd()
+  path <- paste0(path,"/BayesOutput.pdf")
   
-  # Model class
+  if(class(model) != 'stanfit'){
+    stop("model should be an object of class 'stanfit', such as that produced by sawlog::run_classification_engine")
+  }
   
-  x
+  #------------ Plots -------------
   
-  #------------ Render Rmd --------
+  # Posterior predictive checks
   
-  rmarkdown::render(source(system.file("inst/produce_Rmd.R", package = "sawlog")), "pdf_document")
+  color_scheme_set("red")
+  p <- bayesplot::pp_check(model, type = "bars", nsamples = 100) +
+    ggplot2::labs(title = "Posterior predictive check",
+                  x = "Group",
+                  y = "Count")
   
+  p1 <- pp_check(model, type = "ecdf_overlay", nsamples = 100) +
+    ggplot2::labs(title = "Posterior predictive check of cumulative probability function",
+                  x = "Group",
+                  y = "Cumulative Probability")
+  
+  # Chain convergence
+  
+  color_scheme_set("mix-blue-red")
+  p2 <- bayesplot::mcmc_trace(model)
+  
+  # Leave-one-out
+  
+  
+  
+  # Coefficients
+  
+  color_scheme_set("red")
+  p4 <- bayesplot::mcmc_hist(m2) +
+    ggplot2::labs(title = "Coefficient posterior distributions") +
+    ggplot2::geom_vline(xintercept = 0, lty = "dashed", colour = "black", size = 1)
+  
+  #------------ Render ------------
+  
+  CairoPNG(path,11,8)
+  print(p4)
+  print(p)
+  print(p1)
+  print(p2)
+  dev.off()
 }
