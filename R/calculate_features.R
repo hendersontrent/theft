337de9590f988e04e7f60c020da72189405aa282
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-03-28 23:17:46 +1100

    Feature extraction function fix

--- R/calculate_features.R
+++ R/calculate_features.R
@@ -1,31 +1,150 @@
 #------------------- Helper functions to reduce length -------------
 
-calc_feasts <- function(data){
+# catch22
+
+calc_catch22 <- function(data, id, group, time, value){
   
-  tmp_dat <- list(data)
+  storage <- list()
+  ids <- unique(data$id)
   
-  # Calculate features
+  for(i in ids){
+    
+    tsPrep <- data %>%
+      dplyr::filter(id == i) %>%
+      dplyr::arrange(timepoint)
+    
+    tsData <- tsPrep$value
+    
+    # Feature calcs
+    
+    tmp <- catchEmAll::catch22_all(tsData) %>%
+      dplyr::mutate(id = i,
+                    method = "catch22")
+    
+    storage[[i]] <- tmp
+  }
   
-  outData <- features(data)
+  # Pull into one tidy dataframe
   
-  # Wrangle dataframe into format consistent with {catchEmAll}
-  
-  outData <- outData
+  outData <- data.table::rbindlist(storage, use.names = TRUE)
   
   return(outData)
 }
 
-calc_tsfeatures <- function(data){
+# catchaMouse16
+
+calc_catchaMouse16 <- function(data, id, group, time, value){
   
-  tmp_dat <- list(data)
+  storage <- list()
+  ids <- unique(data$id)
   
-  # Calculate features
+  for(i in ids){
+    
+    tsPrep <- data %>%
+      dplyr::filter(id == i) %>%
+      dplyr::arrange(timepoint)
+    
+    tsData <- tsPrep$value
+    
+    # Feature calcs
+    
+    tmp <- catchEmAll::catchaMouse16_all(tsData) %>%
+      dplyr::mutate(id = i,
+                    method = "catchaMouse16")
+    
+    storage[[i]] <- tmp
+  }
   
-  outData <- tsfeatures::tsfeatures(data)
+  # Pull into one tidy dataframe
   
-  # Wrangle dataframe into format consistent with {catchEmAll}
+  outData <- data.table::rbindlist(storage, use.names = TRUE)
   
-  outData <- outData
+  return(outData)
+}
+
+# feasts
+
+calc_feasts <- function(data, id, group, time, value){
+  
+  storage <- list()
+  ids <- unique(data$id)
+  
+  for(i in ids){
+    
+    tsPrep <- data %>%
+      dplyr::filter(id == i) %>%
+      dplyr::arrange(time)
+    
+    tsData <- tsibble::as_tsibble(d, key = id, index = time)
+    
+    # Feature calcs
+    
+    tmp_acf <- tsData %>%
+      fabletools::features(value, feasts::feat_acf) %>%
+      dplyr::mutate(id = id) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values")
+    
+    tmp_int <- tsData %>%
+      fabletools::features(value, feasts::feat_intermittent) %>%
+      dplyr::mutate(id = id) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values")
+    
+    tmp_pacf <- tsData %>%
+      fabletools::features(value, feasts::feat_pacf) %>%
+      dplyr::mutate(id = id) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values")
+    
+    tmp_spec <- tsData %>%
+      fabletools::features(value, feasts::feat_spectral) %>%
+      dplyr::mutate(id = id) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values")
+    
+    tmp_stl <- tsData %>%
+      fabletools::features(value, feasts::feat_stl) %>%
+      dplyr::mutate(id = id) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values")
+    
+    tmp <- dplyr::bind_rows(tmp_acf, tmp_int, tmp_pacf, tmp_spec, tmp_stl) %>%
+      dplyr::mutate(method = "feasts")
+    
+    storage[[i]] <- tmp
+  }
+  
+  # Pull into one tidy dataframe
+  
+  outData <- data.table::rbindlist(storage, use.names = TRUE)
+  
+  return(outData)
+}
+
+# tsfeatures
+
+calc_tsfeatures <- function(data, id, group, time, value){
+  
+  storage <- list()
+  ids <- unique(data$id)
+  
+  for(i in ids){
+    
+    tsPrep <- data %>%
+      dplyr::filter(id == i) %>%
+      dplyr::arrange(time)
+    
+    tsData <- list(tsPrep$value)
+    
+    # Feature calcs
+    
+    tmp <- tsfeatures::tsfeatures(tsData) %>%
+      dplyr::mutate(id = i) %>%
+      tidyr::pivot_longer(cols = !id, names_to = "names", values_to = "values") %>%
+      dplyr::mutate(method = "tsfeatures")
+    
+    storage[[i]] <- tmp
+  }
+  
+  # Pull into one tidy dataframe
+  
+  outData <- data.table::rbindlist(storage, use.names = TRUE)
   
   return(outData)
 }
@@ -37,85 +156,104 @@ calc_tsfeatures <- function(data){
 #' @import catchEmAll
 #' @import feasts
 #' @import tsfeatures
-#' @param data a numerical time-series input vector
+#' @import tsibble
+#' @importFrom tidyr pivot_longer
+#' @importFrom data.table rbindlist
+#' @importFrom fabletools features
+#' @param data a dataframe with at least 4 columns: id variable, group variable, time variable, value variable
+#' @param id_var a string specifying the ID variable to group data on (if one exists). Defaults to NULL
+#' @param group_var a string specifying the grouping variable that the data aggregates to. Defaults to NULL
 #' @param feature_set The set of time-series features to calculate. Defaults to 'all'
 #' @return object of class DataFrame that contains the summary statistics for each feature
 #' @author Trent Henderson
 #' @export
 #' @examples
-#' data <- 1 + 0.5 * 1:1000 + arima.sim(list(ma = 0.5), n = 1000)
-#' outs <- calculate_features(data, feature_set = "all")
+#' \dontrun{
+#' library(dplyr)
+#' d <- data.frame(value = 1 + 0.5 * 1:1000 + arima.sim(list(ma = 0.5), n = 1000)) %>%
+#' dplyr::mutate(id = 1,
+#'               group = 1,
+#'               timepoint = row_number())
+#' outs <- calculate_features(data = d, id_var = "id", group_var = "group", time_var = "timepoint", value_var = "value", feature_set = "all")
+#' }
 #'
 
-calculate_features <- function(data, feature_set = c("all", "catchEmAll", "catch22", "catchaMouse16", "feasts", "tsfeatures")){
+calculate_features <- function(data, id_var = NULL, group_var = NULL, time_var = NULL, value_var = NULL,
+                               feature_set = c("all", "catch22", "catchaMouse16", "feasts", "tsfeatures")){
+  
+  if(is.null(id_var) | is.null(group_var) | is.null(time_var) | is.null(value_var)){
+    stop("As {tsibble} currently cannot handle numeric vectors, input must be a dataframe with at least 4 columns: id, group, timepoint, value")
+  }
   
   # Make 'all' the default
   
   if(missing(feature_set)){
     feature_set <- "all"
-  } else{
-    feature_set <- match.arg(feature_set)
+  }
+  
+  if(is.null(feature_set)){
+    feature_set <- "all"
   }
   
   #--------- Error catches ---------
   
-  # Input vector
-  
-  if(!is.numeric(data)){
-    stop("data should be a vector of numeric values.")
-  }
-  
-  if(length(data) < 5){
-    stop("length of data is too short to make reliable calculations.")
-  }
-  
   # Method selection
   
-  the_sets <- c("all", "catchEmAll", "catch22", "catchaMouse16", "feasts", "tsfeatures")
+  the_sets <- c("all", "catch22", "catchaMouse16", "feasts", "tsfeatures")
   '%ni%' <- Negate('%in%')
   
   if(feature_set %ni% the_sets){
-    stop("feature_set should be a selection or combination of 'all', 'catchEmAll', 'catch22', 'catchaMouse16', 'feasts' or 'tsfeatures' entered as a single string or vector for multiple.")
+    stop("feature_set should be a selection or combination of 'all', 'catch22', 'catchaMouse16', 'feasts' or 'tsfeatures' entered as a single string or vector for multiple.")
   }
   
   #--------- Feature calcs --------
   
-  if(feature_set == "all"){
+  if("all" %in% feature_set){
     
-    tmp <- catchEmAll::catch22_all(data)
-    tmp1 <- catchEmAll::catchaMouse16_all(data)
-    tmp2 <- calc_feasts(data)
-    tmp3 <- calc_tsfeatures(data)
+    tmp <- calc_catch22(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
+    tmp1 <- calc_catchaMouse16(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
+    tmp2 <- calc_feasts(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
+    tmp3 <- calc_tsfeatures(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
     
     tmp_all <- dplyr::bind_rows(tmp, tmp1, tmp2, tmp3)
   }
   
-  if(feature_set == "catchEmAll"){
+  if("catch22" %in% feature_set){
     
-    tmp <- catchEmAll::catch22_all(data)
-    tmp1 <- catchEmAll::catchaMouse16_all(data)
-    
-    tmp_all <- dplyr::bind_rows(tmp, tmp1)
+    tmp <- calc_catch22(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
   }
   
-  if(feature_set == "catch22"){
+  if("catchaMouse16" %in% feature_set){
     
-    tmp_all <- catchEmAll::catch22_all(data)
+    tmp1 <- calc_catchaMouse16(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
   }
   
-  if(feature_set == "catchaMouse16"){
+  if("feasts" %in% feature_set){
     
-    tmp_all <- catchEmAll::catchaMouse16_all(data)
+    tmp2 <- calc_feasts(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
   }
   
-  if(feature_set == "feasts"){
+  if("tsfeatures" %in% feature_set){
     
-    tmp_all <- calc_feasts(data)
+    tmp3 <- calc_tsfeatures(data = data, id = id_var, group = group_var, time = time_var, value = value_var)
   }
   
-  if(feature_set == "tsfeatures"){
-    
-    tmp_all <- calc_tsfeatures(data)
+  tmp_all <- data.frame()
+  
+  if(exists("tmp")){
+    tmp_all <- dplyr::bind_rows(tmp_all, tmp)
+  }
+  
+  if(exists("tmp1")){
+    tmp_all <- dplyr::bind_rows(tmp_all, tmp1)
+  }
+  
+  if(exists("tmp2")){
+    tmp_all <- dplyr::bind_rows(tmp_all, tmp2)
+  }
+  
+  if(exists("tmp3")){
+    tmp_all <- dplyr::bind_rows(tmp_all, tmp3)
   }
   
   return(tmp_all)
