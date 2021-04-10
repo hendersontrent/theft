fd4711ef9751f1d08384e8947b331f4a49f9f710
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-04-11 00:23:43 +1000

    New rescaling functions and documentation

--- R/theft.R
+++ R/theft.R
@@ -1,7 +1,21 @@
-#' @useDynLib theft
-#' @importFrom Rcpp sourceCpp
+#' Tools for Handling Extraction of Features from Time-series
+#' 
+#' @docType package
+#' @name theft
+#' 
+#' @import dplyr
+#' @importFrom magrittr %>%
+#' @import Rcatch22
+#' @import feasts
+#' @import tsfeatures
+#' @import tsibble
+#' @importFrom scales rescale
+#' @importFrom tidyr pivot_longer
+#' @importFrom data.table rbindlist
+#' @importFrom fabletools features
+#' @importFrom fabletools feature_set
+#' @import tibble
+#' @importFrom tidyr drop_na
+#' @importFrom broom augment
+#' @importFrom broom tidy
 NULL
-
-theft <- function(){
-  print("Read the documentation using ??theft")
-}
