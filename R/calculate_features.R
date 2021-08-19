73d8730fe93f8e81253217d2f4c7fd1fcd711e31
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-19 15:24:55 +1000

    Package loads

--- R/calculate_features.R
+++ R/calculate_features.R
@@ -185,7 +185,9 @@ calc_kats <- function(data){
 #' @import dplyr
 #' @importFrom magrittr %>%
 #' @importFrom tibble as_tibble
+#' @importFrom tidyr gather
 #' @importFrom tidyr pivot_longer
+#' @importFrom tidyr unnest_wider
 #' @import tsibble
 #' @import Rcatch22
 #' @importFrom tsfeatures lumpiness stability max_level_shift max_var_shift max_kl_shift crossing_points flat_spots hurst compengine autocorr_features pred_features station_features dist_features scal_features embed2_incircle firstzero_ac ac_9 firstmin_ac trev_num motiftwo_entro3 binarize_mean walker_propcross localsimple_taures sampen_first sampenc std1st_der spreadrandomlocal_meantaul histogram_mode outlierinclude_mdrmd fluctanal_prop_r1 entropy tsfeatures stl_features acf_features pacf_features holt_parameters hw_parameters heterogeneity nonlinearity arch_stat
