91a0a065cb170a4d887d8a09bc12f35f0b20c64e
Trent Henderson <trent.henderson1@outlook.com>
2021-12-16 21:03:50 +1100

    Started multivariate classifier

---
+++ R/fit_multivariate_classifier.R
@@ -0,0 +1,112 @@
+#' Fit a classifier to feature matrix using all features or by set
+#' @import dplyr
+#' @importFrom magrittr %>%
+#' @import ggplot2
+#' @importFrom tidyr drop_na
+#' @importFrom tidyr pivot_wider
+#' @importFrom tibble rownames_to_column
+#' @importFrom e1071 svm
+#' @importFrom data.table rbindlist
+#' @importFrom stats glm binomial
+#' @param data the dataframe containing the raw feature matrix
+#' @param id_var a string specifying the ID variable to group data on (if one exists). Defaults to "id"
+#' @param group_var a string specifying the grouping variable that the data aggregates to. Defaults to "group"
+#' @param by_set Boolean specifying whether to compute classifiers for each feature set. Defaults to FALSE
+#' @param test_method the algorithm to use for quantifying class separation
+#' @return an object of class dataframe containing results
+#' @author Trent Henderson
+#' @export
+#' @examples
+#' \dontrun{
+#' featMat <- calculate_features(data = simData, 
+#'   id_var = "id", 
+#'   time_var = "timepoint", 
+#'   values_var = "values", 
+#'   group_var = "process", 
+#'   feature_set = "catch22")
+#'   
+#' fit_multivariate_classifier(featMat,
+#'   id_var = "id",
+#'   group_var = "group",
+#'   by_set = FALSE,
+#'   test_method = "linear svm") 
+#' }
+#' 
+
+fit_multivariate_classifier <- function(data, id_var = "id", group_var = "group",
+                                        by_set = FALSE,
+                                        test_method = c("t-test", "binomial logistic", "linear svm", "rbf svm")){
+  
+  #---------- Check arguments ------------
+  
+  expected_cols_1 <- "names"
+  expected_cols_2 <- "values"
+  expected_cols_3 <- "method"
+  the_cols <- colnames(data)
+  '%ni%' <- Negate('%in%')
+  
+  if(expected_cols_1 %ni% the_cols){
+    stop("data should contain at least three columns called 'names', 'values', and 'method'. These are automatically produced by calculate_features(). Please consider running this first and then passing the resultant dataframe in to this function.")
+  }
+  
+  if(expected_cols_2 %ni% the_cols){
+    stop("data should contain at least three columns called 'names', 'values', and 'method'. These are automatically produced by calculate_features(). Please consider running this first and then passing the resultant dataframe in to this function.")
+  }
+  
+  if(expected_cols_3 %ni% the_cols){
+    stop("data should contain at least three columns called 'names', 'values', and 'method'. These are automatically produced by calculate_features(). Please consider running this first and then passing the resultant dataframe in to this function.")
+  }
+  
+  if(!is.numeric(data$values)){
+    stop("'values' column in data should be a numerical vector.")
+  }
+  
+  if(!is.null(id_var) && !is.character(id_var)){
+    stop("id_var should be a string specifying a variable in the input data that uniquely identifies each observation.")
+  }
+  
+  # Set defaults for classification method
+  
+  methods <- c("t-test", "binomial logistic", "linear svm", "rbf svm")
+  
+  if(test_method %ni% methods){
+    stop("test_method should be a single string specification of 't-test', 'binomial logistic', 'linear svm', or 'rbf svm'.")
+  }
+  
+  if(length(test_method) != 1){
+    stop("test_method should be a single string specification of 't-test', 'binomial logistic', 'linear svm', or 'rbf svm'.")
+  }
+  
+  num_classes <- length(unique(data$group)) # Get number of classes in the data
+  
+  if(num_classes == 1){
+    stop("Your data only has one class label. At least two are required to performed analysis.")
+  }
+  
+  if(missing(test_method) && num_classes == 2){
+    test_method <- "t-test"
+    message("test_method is NULL. Running t-test for 2-class problem.")
+  }
+  
+  if(missing(test_method) && num_classes > 2){
+    test_method <- "linear svm"
+    message("test_method is NULL. Running linear svm for multiclass problem.")
+  }
+  
+  if(test_method %in% c("t-test", "binomial logistic") && num_classes > 2){
+    stop("t-test and binomial logistic regression can only be run for 2-class problems.")
+  }
+  
+  #------------- Renaming columns -------------
+  
+  if (is.null(id_var)){
+    stop("Data is not uniquely identifiable. Please add a unique identifier variable.")
+  }
+  
+  if(!is.null(id_var)){
+    data_id <- data %>%
+      dplyr::rename(id = dplyr::all_of(id_var),
+                    group = dplyr::all_of(group_var))
+  }
+  
+}
