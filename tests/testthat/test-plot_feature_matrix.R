05ec0ecdd86629b3ddd465681dae42b2593c143b
Trent Henderson <trent.henderson1@outlook.com>
2022-04-01 15:48:52 +1100

    feature matrix plot tests

--- tests/testthat/test-plot_feature_matrix.R
+++ tests/testthat/test-plot_feature_matrix.R
@@ -1,8 +1,35 @@
 context("test-plot_feature_matrix")
 
-feature_matrix <- calculate_features(data = simData, id_var = "id", time_var = "timepoint", values_var = "values", group_var = "process", feature_set = "catch22", catch24 = FALSE)
+feature_matrix <- calculate_features(data = simData, 
+                                     id_var = "id", 
+                                     time_var = "timepoint", 
+                                     values_var = "values", 
+                                     group_var = "process", 
+                                     feature_set = "catch22", 
+                                     catch24 = FALSE)
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+normed <- normalise_feature_frame(feature_matrix, 
+                                  names_var = "names", 
+                                  values_var = "values", 
+                                  method = c("RobustSigmoid"))
+
+test_that("feature matrix plot", {
+  expect_equal(nrow(feature_matrix),
+               nrow(plot_feature_matrix(feature_matrix, 
+                                        is_normalised = FALSE, 
+                                        id_var = "id", 
+                                        method = "RobustSigmoid", 
+                                        interactive = FALSE)$data))
+  
+  expect_equal(nrow(plot_feature_matrix(normed, 
+                                        is_normalised = TRUE, 
+                                        id_var = "id", 
+                                        method = "RobustSigmoid", 
+                                        interactive = FALSE)$data),
+               nrow(plot_feature_matrix(feature_matrix, 
+                                        is_normalised = FALSE, 
+                                        id_var = "id", 
+                                        method = "RobustSigmoid", 
+                                        interactive = FALSE)$data))
 })
 
