54f4cfaa5e6efdd79af3f52640c679d95d82fb95
Trent Henderson <trent.henderson1@outlook.com>
2023-08-06 23:59:03 +1000

    new workflow

--- R/rescalers.R
+++ R/rescalers.R
@@ -3,8 +3,8 @@
 #' \eqn{z_{i} = \frac{x_{i} - \text{min}(\mathbf{x})}{\text{max}(\mathbf{x}) - \text{min}(\mathbf{x})}}
 #' 
 #' @importFrom scales rescale
-#' @param x \code{numeric vector}
-#' @return \code{numeric vector}
+#' @param x \code{numeric} vector
+#' @return \code{numeric} vector
 #' @author Trent Henderson
 #'
 
@@ -14,13 +14,12 @@ minmax_scaler <- function(x){
   return(x_new)
 }
 
-#' Rescales a numeric vector into z-scores and then into the unit interval [0,1]
+#' Rescales a numeric vector into z-scores
 #' 
 #' \eqn{z_{i} = \frac{x_{i} - \mu}{\sigma}}
 #' 
 #' @importFrom stats sd
-#' @importFrom scales rescale
-#' @param x \code{numeric vector}
+#' @param x \code{numeric} vector
 #' @return \code{numeric} vector
 #' @author Trent Henderson
 #'
@@ -36,9 +35,8 @@ zscore_scaler <- function(x){
 #' \eqn{z_{i} = \left[1 + \exp(-\frac{x_{i} - \mu}{\sigma})\right]^{-1}}
 #' 
 #' @importFrom stats sd
-#' @importFrom scales rescale
-#' @param x \code{numeric vector}
-#' @return \code{numeric vector}
+#' @param x \code{numeric} vector
+#' @return \code{numeric} vector
 #' @author Trent Henderson
 #'
 
@@ -48,14 +46,13 @@ sigmoid_scaler <- function(x){
   return(x_new)
 }
 
-#' Rescales a numeric vector using an outlier-robust Sigmoidal transformation and then into the unit interval [0,1]
+#' Rescales a numeric vector using an outlier-robust Sigmoidal transformation
 #' 
 #' \eqn{z_{i} = \left[1 + \exp\left(-\frac{x_{i} - \text{median}(\mathbf{x})}{\text{IQR}(\mathbf{x})/{1.35}}\right)\right]^{-1}}
 #' 
 #' @importFrom stats median IQR
-#' @importFrom scales rescale
-#' @param x \code{numeric vector}
-#' @return \code{numeric vector}
+#' @param x \code{numeric} vector
+#' @return \code{numeric} vector
 #' @references Fulcher, Ben D., Little, Max A., and Jones, Nick S. Highly Comparative Time-Series Analysis: The Empirical Structure of Time Series and Their Methods. Journal of The Royal Society Interface 10(83), (2013).
 #' @author Trent Henderson
 #'
